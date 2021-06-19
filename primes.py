93599a8416ecb259197c94a6b5896055c2f99568
Malvika Sharma <malvika.sharma@live.com>
2021-06-19 18:51:40 +0530

    optimized code for generating list of prime numbers

--- primes.py
+++ primes.py
@@ -7,8 +7,9 @@ def is_prime(n):
 
     if n == 2:
         return True
-
-    for i in range(2,n):
+    if n < 2:
+        return False
+    for i in range(2,int(n/2)+1):
         if n % i == 0:
             return False
     return True
