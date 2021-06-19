a53af8b52118ccea72a95b3941464f06ea5e225b
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2021-06-19 12:07:27 +0530

    Added primes exercise

---
+++ primes.py
@@ -0,0 +1,15 @@
+# Filter Prime Numbers
+# Loop through the first 1000 numbers and build a list of prime numbers
+# First fill out this function definition to check an individual number then use it later
+
+def is_prime(n):
+    # Write your code here and return False/True
+    return False
+
+
+# Code here to build the list of primes from the first 1000 numbers
+primes = []
+
+for i in range(1000):
+    # Remove the pass and fill this out
+    pass
