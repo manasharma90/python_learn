ecbf801fc4bbad4222ff2ee956ae8e221acaf931
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2021-06-20 15:33:06 +0000

    Added functions exercise

---
+++ functions.py
@@ -0,0 +1,11 @@
+# define a function to check if an input string is a palindrome or not
+def is_Palindrome(s):
+    # if s is a palindrome, return True
+    # if s is not a palindrome, return False
+    pass
+
+
+# define a function to filter even numbers from a given list of numbers
+def filter_even(nums):
+    filtered = []
+    return filtered
