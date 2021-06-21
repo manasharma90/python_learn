05d386283a7ff62d3c17028d9d5e79354d000a10
Malvika Sharma <malvika.sharma@live.com>
2021-06-21 21:24:00 +0530

    added files2

--- files.py
+++ files.py
@@ -1,1 +1,2 @@
 # Read in the contents of the file 'hello.txt' and copy them into a new file 'world.txt'
+# Hint, lookup `with open` syntax.
---
+++ files2.py
@@ -0,0 +1,3 @@
+# Read the contents of 'book.txt' and find the 50 most frequent words.
+# Hint: Read about split(). 
+# Hint: Do not bother if you end up with words such as "Wonderland\n\n". This is acceptable.
