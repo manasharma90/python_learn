c7a6371b60032f5bb32c49b2ed977f47e537cff3
Malvika Sharma <malvika.sharma@live.com>
2021-06-21 21:41:26 +0530

    reading and writing files using with open

--- files.py
+++ files.py
@@ -1,2 +1,9 @@
 # Read in the contents of the file 'hello.txt' and copy them into a new file 'world.txt'
 # Hint, lookup `with open` syntax.
+
+with open('hello.txt', 'r') as f:
+    a = f.read()
+
+with open('world.txt', 'w') as g:
+    g.write(a)
+
