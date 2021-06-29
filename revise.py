05d7623d5dabbd06328e7ad70665695b49d2165e
Malvika Sharma <malvika.sharma@live.com>
2021-06-29 21:16:59 +0530

    sync

---
+++ revise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Your task is to find all the palindromes in the support_files/words_alpha.txt file.
+# This file contains thousands of english words, your program needs to create a list of all the palindromes within this list.
+
+
+import sys
+
+# Use this to perform your calculations
+def find_palindromes(filename):
+    pass
+
+# Make more functions to create smaller reusable pieces of code.
+
+# This is the main function of your program.
+def main(filename):
+    find_palindromes(filename)
+
+# This is boilerplate code to call the main function when this script is run.
+if __name__=="__main__":
+    main(sys.argv[1])
